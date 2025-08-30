7816712a2d5e169b8d6d2f0d61d304cb4ae63373
Nicolas O <nicolasolaya27@gmail.com>
2025-08-30 15:16:50 -0500

    First commit

---
+++ src/main/java/com/eam/mscompaniesmanage/MscompaniesmanageApplication.java
@@ -0,0 +1,13 @@
+package com.eam.mscompaniesmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscompaniesmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscompaniesmanageApplication.class, args);
+	}
+
+}
